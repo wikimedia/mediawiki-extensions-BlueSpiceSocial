3e6f7a36e0ae0a91a4c27cf518bc2c16f2acabe4
Pwirth <wirth@hallowelt.biz>
2019-06-03 13:16:56 +0000

    Use new modular lockdown. ERM:15194
    [3.1]
    Change-Id: I687da1c547c4ac57f4307dc2d31d5039a236a2e2

---
+++ src/Permission/Lockdown/Module/ReadPermissionCheckHandOver.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace BlueSpice\Social\Permission\Lockdown\Module;
+
+use IContextSource;
+use Config;
+use Message;
+use Title;
+use User;
+use BlueSpice\Services;
+use BlueSpice\EntityFactory;
+use BlueSpice\Social\Entity;
+
+class ReadPermissionCheckHandOver extends \BlueSpice\Permission\Lockdown\Module {
+
+	/**
+	 *
+	 * @var EntityFactory
+	 */
+	protected $entityFactory = null;
+
+	/**
+	 *
+	 * @param Config $config
+	 * @param IContextSource $context
+	 * @param Services $services
+	 * @param EntityFactory $entityFactory
+	 */
+	protected function __construct( Config $config, IContextSource $context, Services $services,
+		EntityFactory $entityFactory ) {
+		parent::__construct( $config, $context, $services );
+
+		$this->entityFactory = $entityFactory;
+	}
+
+	/**
+	 *
+	 * @param Config $config
+	 * @param IContextSource $context
+	 * @param Services $services
+	 * @param EntityFactory $entityFactory
+	 * @return \static
+	 */
+	public static function getInstance( Config $config, IContextSource $context,
+		Services $services, array $entityFactory = null ) {
+
+		if ( !$entityFactory ) {
+			$entityFactory = $services->getBSEntityFactory();
+		}
+
+		return new static(
+			$config,
+			$context,
+			$services,
+			$entityFactory
+		);
+	}
+
+	/**
+	 *
+	 * @param Title $title
+	 * @param User $user
+	 * @return bool
+	 */
+	public function applies( Title $title, User $user ) {
+		return $title->getNamespace() === NS_SOCIALENTITY;
+	}
+
+	/**
+	 *
+	 * @param Title $title
+	 * @param User $user
+	 * @param string $action
+	 * @return bool
+	 */
+	public function mustLockdown( Title $title, User $user, $action ) {
+		if( $action !== 'read' ) {
+			return false;
+		}
+		$entity = $this->entityFactory->newFromSourceTitle( $title );
+		if( !$entity instanceof Entity ) {
+			return false;
+		}
+		return !$entity->userCan()->isOK();
+	}
+
+	/**
+	 *
+	 * @param Title $title
+	 * @param User $user
+	 * @param string $action
+	 * @return Message
+	 */
+	public function getLockdownReason( Title $title, User $user, $action ) {
+		$entity = $this->entityFactory->newFromSourceTitle( $title );
+		return $entity->userCan()->getMessage();
+	}
+
+}
---
+++ src/Permission/Lockdown/Module/SourceTitle.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace BlueSpice\Social\Permission\Lockdown\Module;
+
+use Message;
+use Title;
+use User;
+
+class SourceTitle extends \BlueSpice\Permission\Lockdown\Module {
+
+	/**
+	 *
+	 * @param Title $title
+	 * @param User $user
+	 * @return bool
+	 */
+	public function applies( Title $title, User $user ) {
+		return $title->getNamespace() === NS_SOCIALENTITY;
+	}
+
+	/**
+	 *
+	 * @param Title $title
+	 * @param User $user
+	 * @param string $action
+	 * @return bool
+	 */
+	public function mustLockdown( Title $title, User $user, $action ) {
+		if( $action === 'read' ) {
+			return false;
+		}
+		//No one should be able to modify this articles besides a sysop
+		return $action !== 'wikiadmin' && !$user->isAllowed( 'wikiadmin' );
+	}
+
+	/**
+	 *
+	 * @param Title $title
+	 * @param User $user
+	 * @param string $action
+	 * @return Message
+	 */
+	public function getLockdownReason( Title $title, User $user, $action ) {
+		$actionMsg = $this->msg( "right-$action" );
+		return $this->msg(
+			'bs-social-sourcetitle-lockdown-reason-reason',
+			$actionMsg->exists() ? $actionMsg : $action
+		);
+	}
+
+}
