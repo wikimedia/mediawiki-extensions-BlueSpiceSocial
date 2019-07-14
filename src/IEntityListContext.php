45f3ce9b915d6316438e06617a7e5c807d2185a5
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2019-07-14 09:16:09 +0000

    build: Updating dependencies
    composer:
    * mediawiki/mediawiki-codesniffer: 25.0.0 → 26.0.0
    
    npm:
    * grunt: 1.0.3 → 1.0.4
      * https://npmjs.com/advisories/788
      * https://npmjs.com/advisories/813
    * grunt: 1.0.4 → 1.0.4
    
    Additional changes:
    * Committed package-lock.json (T179229) too.
    
    Change-Id: I28d982935697a7c39a2bef2284e9bf3e011577ee

--- src/IEntityListContext.php
+++ src/IEntityListContext.php
@@ -10,82 +10,102 @@ interface IEntityListContext extends IContextSource {
 	 * @return int
 	 */
 	public function getLimit();
+
 	/**
 	 * @return int
 	 */
 	public function getStart();
+
 	/**
 	 * @return array
 	 */
 	public function getSort();
+
 	/**
 	 * @return array
 	 */
 	public function getFilters();
+
 	/**
 	 * @return string[]
 	 */
 	public function getAvailableFilterFields();
+
 	/**
 	 * @return string[]
 	 */
 	public function getAvailableSorterFields();
+
 	/**
 	 * @return string[]
 	 */
 	public function getAllowedTypes();
+
 	/**
 	 * @return bool
 	 */
 	public function showEntityListMenu();
+
 	/**
 	 * @return bool
 	 */
 	public function showEntityListMore();
+
 	/**
 	 * @return bool
 	 */
 	public function showEntitySpawner();
+
 	/**
 	 * @return bool
 	 */
 	public function showHeadline();
+
 	/**
 	 * @return string
 	 */
 	public function getHeadlineMessageKey();
+
 	/**
 	 * @return bool
 	 */
 	public function useEndlessScroll();
+
 	/**
 	 * @return array
 	 */
 	public function getOutputTypes();
+
 	/**
 	 * @return array
 	 */
 	public function getPreloadTitles();
+
 	/**
 	 * @return string[]
 	 */
 	public function getLockedOptionNames();
+
 	/**
 	 * @return string[]
 	 */
 	public function getLockedFilterNames();
+
 	/**
 	 * @return Schema
 	 */
 	public function getSchema();
+
 	/**
 	 * @return Entity|null
 	 */
 	public function getParent();
+
 	/**
 	 * @return \stdClass[]
 	 */
 	public function getPreloadedEntities();
+
 	/**
 	 * @return sring
 	 */
