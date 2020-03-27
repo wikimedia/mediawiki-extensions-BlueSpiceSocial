151a459c37fdf36508b3f71c5ef165a6836359b2
alexpin <intracomof@gmail.com>
2020-03-28 03:09:46 +0700

    Social Entities filtering
    ERM 18457
    
    Change-Id: I48afe311c28990c345c261d62bb5007b68ab0769

---
+++ src/Hook/ChangesListSpecialPageStructuredFilters/AddSocialEntityFilter.php
@@ -0,0 +1,42 @@
+<?php
+namespace BlueSpice\Social\Hook\ChangesListSpecialPageStructuredFilters;
+
+use BlueSpice\Hook\ChangesListSpecialPageStructuredFilters;
+use ChangesListBooleanFilterGroup;
+
+class AddSocialEntityFilter extends ChangesListSpecialPageStructuredFilters {
+
+	/**
+	 * @return bool
+	 */
+	public function doProcess() {
+		$socialActionsGroup = new ChangesListBooleanFilterGroup(
+			$this->getSocialEntitiesFilterDefinition()
+		);
+		$this->specialPage->registerFilterGroup( $socialActionsGroup );
+		return true;
+	}
+
+	/**
+	 * @return array
+	 */
+	private function getSocialEntitiesFilterDefinition() {
+		return [
+			'name' => 'socialentities',
+			'class' => ChangesListBooleanFilterGroup::class,
+			'filters' => [
+				[
+					'name' => 'hidesocialentities',
+					'showHide' => 'bs-social-specialpagelisting-socialentities',
+					'default' => true,
+					'queryCallable' => function (
+						$specialClassName, $ctx, $dbr, &$tables, &$fields,
+						&$conds, &$query_options, &$join_conds
+					) {
+						$conds[] = 'rc_namespace != ' . NS_SOCIALENTITY;
+					}
+				]
+			]
+		];
+	}
+}
