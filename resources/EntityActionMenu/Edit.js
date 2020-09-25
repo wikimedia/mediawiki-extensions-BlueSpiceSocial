d60216d5677a0e93a12ea58917e63d54dfbc4b58
skuehn <stefan@ws12.de>
2020-09-25 14:01:06 +0200

    Implement UX review
    Redisign the discussion area
    
    ERM#20227
    [3.3]
    
    Change-Id: Ie0f664f79e90e62ff717e26161e135498e8e5755

---
+++ resources/EntityActionMenu/Edit.js
@@ -0,0 +1,36 @@
+/*
+* @author     Stefan Kühn
+* @package    BluespiceSocial
+* @subpackage BlueSpiceSocial
+* @copyright  Copyright (C) 2020 Hallo Welt! GmbH, All rights reserved.
+* @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
+*/
+
+bs.social = bs.social || {};
+bs.social.EntityActionMenu = bs.social.EntityActionMenu || {};
+
+bs.social.EntityActionMenu.Edit = function ( entityActionMenu ) {
+	OO.EventEmitter.call( this );
+	var me = this;
+	me.entityActionMenu = entityActionMenu;
+	me.$element = null;
+	me.priority = 20;
+	me.$element = $( '<li><a class="dropdown-item">'
+		+ mw.message( "bs-social-entityaction-edit").plain()
+		+ '</a></li>'
+	);
+	me.$element.on( 'click', function() { me.click(); } );
+};
+
+OO.initClass( bs.social.EntityActionMenu.Edit );
+OO.mixinClass( bs.social.EntityActionMenu.Edit, OO.EventEmitter );
+
+bs.social.EntityActionMenu.Edit.prototype.click = function ( e ) {
+	if( this.entityActionMenu.entity.editmode ) {
+		e.preventDefault();
+		return false;
+	}
+	this.entityActionMenu.entity.makeEditMode();
+	e.preventDefault();
+	return false;
+};
