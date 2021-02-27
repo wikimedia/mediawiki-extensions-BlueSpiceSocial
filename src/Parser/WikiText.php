b1b124b00cd66880aa6512daa3355ebfee0f4b77
Daimona Eaytoy <daimona.wiki@gmail.com>
2021-02-27 14:03:38 +0000

    Delete blocks of commented code
    This shows up in codesearch, and generally makes maintenance more
    difficult. This code has been commented out for almost two years
    (initial commit), so it seems like it's not really needed.
    Especially because the source of this code was updated in the
    meanwhile, so it would need updating anyway. Also, it's still
    available via git, if necessary.
    
    Change-Id: Ifa3e19fd0ccee20942f1c817ecc4df4b611eaf87

--- src/Parser/WikiText.php
+++ src/Parser/WikiText.php
@@ -21,19 +21,14 @@ class WikiText extends \Parser {
 	 * @return string
 	 */
 	public function parse( $text, \Title $title, \ParserOptions $options, $linestart = true,
-		$clearState = true, $revid = null ) {
-		/*if ( $clearState ) {
-			$magicScopeVariable = $this->lock();
-		}*/
-
+		$clearState = true, $revid = null
+	) {
 		$this->setTitle( $title );
 		$this->mOptions = $options;
 		$this->setOutputType( self::OT_HTML );
 		if ( $clearState ) {
 			$this->clearState();
 		}
-
-		// $this->startParse( $title, $options, self::OT_HTML, $clearState );
 
 		$this->mInputSize = strlen( $text );
 		if ( $this->mOptions->getEnableLimitReport() ) {
@@ -58,11 +53,8 @@ class WikiText extends \Parser {
 			$this->mRevisionSize = null;
 		}
 
-		// Hooks::run( 'ParserBeforeStrip', array( &$this, &$text, &$this->getStripState() ) );
 		# No more strip!
-		// Hooks::run( 'ParserAfterStrip', array( &$this, &$text, &$this->getStripState() ) );
 		$text = $this->internalParse( $text );
-		// Hooks::run( 'ParserAfterParse', array( &$this, &$text, &$this->getStripState() ) );
 
 		$text = $this->getStripState()->unstripGeneral( $text );
 
@@ -84,128 +76,10 @@ class WikiText extends \Parser {
 
 		$this->replaceLinkHolders( $text );
 
-		/**
-		 * The input doesn't get language converted if
-		 * a) It's disabled
-		 * b) Content isn't converted
-		 * c) It's a conversion table
-		 * d) it is an interface message (which is in the user language)
-		 */
-		/*if ( !( $options->getDisableContentConversion()
-			|| isset( $this->mDoubleUnderscores['nocontentconvert'] ) )
-		) {
-			if ( !$this->mOptions->getInterfaceMessage() ) {
-				# The position of the convert() call should not be changed. it
-				# assumes that the links are all replaced and the only thing left
-				# is the <nowiki> mark.
-				$text = $this->getTargetLanguage()->convert( $text );
-			}
-		}*/
-
-		/**
-		 * A converted title will be provided in the output object if title and
-		 * content conversion are enabled, the article text does not contain
-		 * a conversion-suppressing double-underscore tag, and no
-		 * {{DISPLAYTITLE:...}} is present. DISPLAYTITLE takes precedence over
-		 * automatic link conversion.
-		 */
-		/*if ( !( $options->getDisableTitleConversion()
-			|| isset( $this->mDoubleUnderscores['nocontentconvert'] )
-			|| isset( $this->mDoubleUnderscores['notitleconvert'] )
-			|| $this->mOutput->getDisplayTitle() !== false )
-		) {
-			$convruletitle = $this->getTargetLanguage()->getConvRuleTitle();
-			if ( $convruletitle ) {
-				$this->mOutput->setTitleText( $convruletitle );
-			} else {
-				$titleText = $this->getTargetLanguage()->convertTitle( $title );
-				$this->mOutput->setTitleText( $titleText );
-			}
-		}
-
-		$text = $this->getStripState()->unstripNoWiki( $text );
-
-		Hooks::run( 'ParserBeforeTidy', array( &$this, &$text ) );
-
-		$text = $this->getStripState()->unstripGeneral( $text );
-*/
 		$text = \Sanitizer::normalizeCharReferences( $text );
 
 		$text = MWTidy::tidy( $text );
 
-		/*if ( $this->mExpensiveFunctionCount > $this->mOptions->getExpensiveParserFunctionLimit() ) {
-			$this->limitationWarn( 'expensive-parserfunction',
-				$this->mExpensiveFunctionCount,
-				$this->mOptions->getExpensiveParserFunctionLimit()
-			);
-		}
-
-		Hooks::run( 'ParserAfterTidy', array( &$this, &$text ) );
-*/
-		# Information on include size limits, for the benefit of users who try to skirt them
-		/*if ( $this->mOptions->getEnableLimitReport() ) {
-			$max = $this->mOptions->getMaxIncludeSize();
-
-			$cpuTime = $this->mOutput->getTimeSinceStart( 'cpu' );
-			if ( $cpuTime !== null ) {
-				$this->mOutput->setLimitReportData( 'limitreport-cputime',
-					sprintf( "%.3f", $cpuTime )
-				);
-			}
-
-			$wallTime = $this->mOutput->getTimeSinceStart( 'wall' );
-			$this->mOutput->setLimitReportData( 'limitreport-walltime',
-				sprintf( "%.3f", $wallTime )
-			);
-
-			$this->mOutput->setLimitReportData( 'limitreport-ppvisitednodes',
-				array( $this->mPPNodeCount, $this->mOptions->getMaxPPNodeCount() )
-			);
-			$this->mOutput->setLimitReportData( 'limitreport-postexpandincludesize',
-				array( $this->mIncludeSizes['post-expand'], $max )
-			);
-			$this->mOutput->setLimitReportData( 'limitreport-templateargumentsize',
-				array( $this->mIncludeSizes['arg'], $max )
-			);
-			$this->mOutput->setLimitReportData( 'limitreport-expansiondepth',
-				array( $this->mHighestExpansionDepth, $this->mOptions->getMaxPPExpandDepth() )
-			);
-			$this->mOutput->setLimitReportData( 'limitreport-expensivefunctioncount',
-				array( $this->mExpensiveFunctionCount, $this->mOptions->getExpensiveParserFunctionLimit() )
-			);
-			Hooks::run( 'ParserLimitReportPrepare', array( $this, $this->mOutput ) );
-
-			$limitReport = "NewPP limit report\n";
-			if ( $wgShowHostnames ) {
-				$limitReport .= 'Parsed by ' . wfHostname() . "\n";
-			}
-			foreach ( $this->mOutput->getLimitReportData() as $key => $value ) {
-				if ( Hooks::run( 'ParserLimitReportFormat',
-					array( $key, &$value, &$limitReport, false, false )
-				) ) {
-					$keyMsg = wfMessage( $key )->inLanguage( 'en' )->useDatabase( false );
-					$valueMsg = wfMessage( array( "$key-value-text", "$key-value" ) )
-						->inLanguage( 'en' )->useDatabase( false );
-					if ( !$valueMsg->exists() ) {
-						$valueMsg = new RawMessage( '$1' );
-					}
-					if ( !$keyMsg->isDisabled() && !$valueMsg->isDisabled() ) {
-						$valueMsg->params( $value );
-						$limitReport .= "{$keyMsg->text()}: {$valueMsg->text()}\n";
-					}
-				}
-			}
-			// Since we're not really outputting HTML, decode the entities and
-			// then re-encode the things that need hiding inside HTML comments.
-			$limitReport = htmlspecialchars_decode( $limitReport );
-			Hooks::run( 'ParserLimitReport', array( $this, &$limitReport ) );
-
-			// Sanitize for comment. Note '‐' in the replacement is U+2010,
-			// which looks much like the problematic '-'.
-			$limitReport = str_replace( array( '-', '&' ), array( '‐', '&amp;' ), $limitReport );
-			$text .= "\n<!-- \n$limitReport-->\n";
-
-		}*/
 		$this->mOutput->setText( $text );
 
 		$this->mRevisionId = $oldRevisionId;
