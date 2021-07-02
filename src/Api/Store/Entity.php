f744798b525c6bbbec26b6b1d5eab2f2b6c8eb14
pwirth <wirth@hallowelt.biz>
2021-07-02 13:14:35 +0200

    Add user timestamp to UTC conversion for timestampcreated filter
    => requires: I3ff913e030fa1d5a729bae6516ced51d2ec79a60
    
    ERM:19966
    [3.2.x]
    
    Depends-on: I3ff913e030fa1d5a729bae6516ced51d2ec79a60
    Change-Id: I8197b25d3bf4b3eb4e0b86aa8765e9d1a1145948

--- src/Api/Store/Entity.php
+++ src/Api/Store/Entity.php
@@ -2,9 +2,53 @@
 
 namespace BlueSpice\Social\Api\Store;
 
+use BlueSpice\Data\Filter;
+use BlueSpice\Data\Filter\Date;
+use BlueSpice\Data\FilterFactory;
+use BlueSpice\Data\ReaderParams;
+use BlueSpice\Social\Entity as SocialEntity;
+use BlueSpice\Timestamp;
+
 class Entity extends \BlueSpice\Api\Store {
 
 	protected function makeDataStore() {
 		return new \BlueSpice\Social\Data\Entity\Store( $this->getContext() );
+	}
+
+	/**
+	 *
+	 * @return ReaderParams
+	 */
+	protected function getReaderParams() {
+		$readerParams = parent::getReaderParams();
+		$filters = $readerParams->getFilter();
+		foreach ( $filters as &$filter ) {
+			if ( $filter->{Date::KEY_PROPERTY} === SocialEntity::ATTR_TIMESTAMP_CREATED ) {
+				// reset creation filter timestamp to UTC
+				$filter->{Date::KEY_COMPARISON} = Date::COMPARISON_GREATER_THAN;
+				$ts = new Timestamp( $filter->{Date::KEY_VALUE} );
+				$ts->unOffsetForUser( $this->getUser() );
+				$filter->{Date::KEY_VALUE} = $ts->format( 'YmdHis' );
+				break;
+			}
+		}
+		$invertedTypeMap = array_flip( FilterFactory::getTypeMap() );
+		$newFilters = [];
+		foreach ( $filters as $filter ) {
+			$newFilters[] = [
+				Filter::KEY_TYPE => $invertedTypeMap[get_class( $filter )],
+				Filter::KEY_FIELD => $filter->getField(),
+				Filter::KEY_COMPARISON => $filter->getComparison(),
+				Filter::KEY_VALUE => $filter->getValue()
+			];
+		}
+
+		return new ReaderParams( [
+			ReaderParams::PARAM_QUERY => $readerParams->getQuery(),
+			ReaderParams::PARAM_START => $readerParams->getStart(),
+			ReaderParams::PARAM_LIMIT => $readerParams->getLimit(),
+			ReaderParams::PARAM_SORT => $readerParams->getSort(),
+			ReaderParams::PARAM_FILTER => $newFilters,
+		] );
 	}
 }
