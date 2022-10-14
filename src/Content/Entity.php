d3d6915def6d04d8295f07882442b01fa38d3567
Markus Glaser <glaser@hallowelt.com>
2022-10-14 14:23:15 +0200

    Fix deprecated preSaveTransform override
    [4.x]
    
    Change-Id: I6e860f8233828eee72d1b0ba3c453af4958fd233
    (cherry picked from commit 4652c9b8da7b9ff004dda6fc3bfb8208caa67abd)

--- src/Content/Entity.php
+++ src/Content/Entity.php
@@ -41,18 +41,6 @@ class Entity extends \BlueSpice\Content\Entity {
 	}
 
 	/**
-	 * Beautifies JSON prior to save.
-	 *
-	 * @param Title $title Title
-	 * @param User $user User
-	 * @param ParserOptions $popts
-	 * @return JsonContent
-	 */
-	public function preSaveTransform( \Title $title, \User $user, \ParserOptions $popts ) {
-		return new static( $this->beautifyJSON() );
-	}
-
-	/**
 	 * Constructs an HTML representation of a JSON object.
 	 *
 	 * @param array $mapping
