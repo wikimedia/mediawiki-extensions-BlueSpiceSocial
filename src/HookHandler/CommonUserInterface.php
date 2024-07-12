6913ad3771dd74221af377c02eec541267081eb6
Mschlindwein <schlindwein@hallowelt.com>
2024-07-12 12:41:34 +0200

    Add less vars to hook
    ERM37942
    
    Depends-On: https://github.com/hallowelt/mwstake-mediawiki-component-commonuserinterface/pull/43
    
    [4.5.x]
    
    Change-Id: I4737fc75b9cbb0a7b640e5cd7202e9db986bcb2c
    (cherry picked from commit dc10685a58f2ae07e4cdb8112f27f801c97590bc)
    (cherry picked from commit 2aff40c09cf003afd56fccc2de117211522934d3)

--- src/HookHandler/CommonUserInterface.php
+++ src/HookHandler/CommonUserInterface.php
@@ -4,9 +4,10 @@ namespace BlueSpice\Social\HookHandler;
 
 use BlueSpice\Social\MainLinkPanel;
 use ConfigFactory;
+use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUILessVarsInit;
 use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
 
-class CommonUserInterface implements MWStakeCommonUIRegisterSkinSlotComponents {
+class CommonUserInterface implements MWStakeCommonUIRegisterSkinSlotComponents, MWStakeCommonUILessVarsInit {
 
 	/**
 	 * @var ConfigFactory
@@ -38,5 +39,30 @@ class CommonUserInterface implements MWStakeCommonUIRegisterSkinSlotComponents {
 				]
 			);
 		}
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onMWStakeCommonUILessVarsInit( $lessVars ): void {
+		$lessVars->setVar( 'bs-primary-light-background', '#6E7B96' );
+		$lessVars->setVar( 'bs-light-background', 'lighten(@bs-color-neutral, 38.43%)' );
+		$lessVars->setVar( 'bs-primary-light-subelement-background', 'lighten(@bs-primary-light-background, 24.3%)' );
+		$lessVars->setVar( 'bs-tertiary-light-background', 'lighten(@bs-color-tertiary, 45%)' );
+		$lessVars->setVar( 'bs-color-neutral-headline', 'lighten(@bs-color-neutral, 27.8%)' );
+		$lessVars->setVar( 'bs-color-lighten-information', '#747475' );
+		$lessVars->setVar( 'bs-color-header-information', '#252525' );
+		$lessVars->setVar( 'bs-color-header-information-link', '@bs-primary-light-subelement-border' );
+		$lessVars->setVar( 'bs-color-social-entity-background', 'white' );
+		$lessVars->setVar( 'bs-color-social-link', '#0060DF' );
+		$lessVars->setVar( 'bs-color-social-link-dark', '@bs-primary-light-subelement-border' );
+		$lessVars->setVar( 'navigation-tab-color', '@bs-color-primary' );
+		$lessVars->setVar( 'navigation-color', 'lighten( @bs-color-neutral3, 23.1373)' );
+		$lessVars->setVar( 'bs-social-background-color-archived', '#F1D8D8' );
+		$lessVars->setVar(
+			'bs-social-background-color-owned',
+			'lighten(desaturate(spin(@bs-primary-light-background, 0.4545), 2.1605), 27.0588)'
+		);
+		$lessVars->setVar( 'bs-primary-light-subelement-border', '#3E5389' );
 	}
 }
