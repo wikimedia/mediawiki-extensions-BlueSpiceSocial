a4711e45e46f1da9d5f2ff1de1dc2c1fdb600dc7
d.savuljesku <d.savuljesku@gmail.com>
2024-10-15 10:01:30 +0200

    Fix action buttons
    HTML cannot be concatenated with single quotes like this in JS
    
    [4.5.x]
    
    ERM39024
    
    Change-Id: I55ab880f5a5009a9dd16edae5739d6f725df7b2d
    (cherry picked from commit 796445d7907abe4ffe88c6064f678209f9735b5b)

--- resources/EntityActionMenu/Edit.js
+++ resources/EntityActionMenu/Edit.js
@@ -15,11 +15,14 @@ bs.social.EntityActionMenu.Edit = function ( entityActionMenu ) {
 	me.entityActionMenu = entityActionMenu;
 	me.$element = null;
 	me.priority = 4;
-	me.$element = $( '<li><a class="dropdown-item bs-social-entity-action-edit" tabindex="0" role="button"'
-		+ 'aria-label=' + mw.message( "bs-social-entityaction-edit" ).plain() + '>'
-		+ mw.message( "bs-social-entityaction-edit" ).plain() +
-		+ '</a></li>'
-	);
+	me.$element = $( '<li></li>' );
+	var $a = $( '<a>' )
+		.addClass( 'dropdown-item bs-social-entity-action-edit' )
+		.attr( 'tabindex', 0 )
+		.attr( 'role', 'button' )
+		.attr( 'aria-label', mw.message( 'bs-social-entityaction-edit' ).plain() )
+		.text( mw.message( 'bs-social-entityaction-edit' ).plain() );
+	me.$element.append( $a );
 	me.$element.on( 'click', function( e ) { me.click( e ); } );
 };
 
